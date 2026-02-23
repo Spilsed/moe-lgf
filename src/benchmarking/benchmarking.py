e8ff8733880c3b2467c7a7fa1b05a717c72abf86
bean-enjoyer <perryc4@rpi.edu>
2026-02-23 17:39:39 -0500

    added more arguments, and removed automatic thinking

--- src/benchmarking/benchmarking.py
+++ src/benchmarking/benchmarking.py
@@ -4,8 +4,12 @@ from datasets import load_dataset
 import sys,time
 
 ollamaOn=False
+thinking=False
 model=""
 shard_count=10
+split="test"
+subset="ARC-Challenge"
+dataset="allenai/ai2_arc"
 sysPrompt="""
 Choose the answer which is most correct.
 Respond only with the label of the choice in the final answer.
@@ -17,29 +21,26 @@ if '-o' in sys.argv:
     model=sys.argv[1+sys.argv.index("-o")]
 if '-s' in sys.argv:
     shard_count=int(sys.argv[1+sys.argv.index("-s")])
+if '-t' in sys.argv:
+    thinking=True
+if '-s' in sys.argv:
+    split=sys.argv[1+sys.argv.index("-s")]
+if '-ss' in sys.argv:
+    subset=sys.argv[1+sys.argv.index("-ss")]
+if '-ds' in sys.argv:
+    dataset=sys.argv[1+sys.argv.index("-ds")]
 
 
 def send_and_grade(model: str, question: str, choices: dict[str:list[str]], answer: str):
     q=question+'\n'+'\n'.join(map(': '.join, zip(choices["label"],choices["text"])))
     print(q.strip())
     if ollamaOn:
-        try:
-            response: ChatResponse = chat(model=model,messages=[
-                {
-                    "role": "user",
-                    "content": sysPrompt+q
-                }
-            ],think=True)
-        except ResponseError as e:
-            if e.status_code==400:
-                response: ChatResponse = chat(model=model,messages=[
-                {
-                    "role": "user",
-                    "content": sysPrompt+q
-                }
-            ])
-            else:
-                raise(e.error)
+        response: ChatResponse = chat(model=model,messages=[
+            {
+                "role": "user",
+                "content": sysPrompt+q
+            }
+        ],think=thinking)
         print(model,"response:",response.message.content.strip())
         if response.message.content.strip().startswith(answer):
             print("correct")
@@ -48,7 +49,7 @@ def send_and_grade(model: str, question: str, choices: dict[str:list[str]], answ
             print("incorrect: correct answer was",answer)
     return False
 
-data = load_dataset("allenai/ai2_arc","ARC-Challenge",split="validation").remove_columns("id").shuffle(time.time_ns()).shard(shard_count,0)
+data = load_dataset(dataset,subset,split=split).remove_columns("id").shuffle(time.time_ns()).shard(shard_count,0)
 questions = data["question"]
 choices = data["choices"]
 answers = data["answerKey"]
