090c3a5e9b27c73e7d31583e1dff94266cda0300
bean-enjoyer <perryc4@rpi.edu>
2026-04-23 10:59:57 -0400

    Add support for Rowan/hellaswag

--- src/benchmarking/benchmarking.py
+++ src/benchmarking/benchmarking.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
-from ollama import chat, ChatResponse, ResponseError
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from accelerate import Accelerator
 import signal,sys,time,torch
 
-torch.set_num_threads(12)
+torch.set_num_threads(14)
 
-ollamaOn=False
-thinking=False
 model=""
 num_rows=100
 whole_set=False
@@ -18,24 +15,24 @@ dataset="allenai/ai2_arc"
 sysPrompt="""
 Choose the answer which is most correct.
 Respond only with the label of the choice in the final answer.
-Do not respond with any other text such as 'the correct answer is' or the text of the answer, ONLY the label(A, B, 3, etc.)
+Do not respond with any other text such as 'the correct answer is' or the text of the answer, ONLY the label(A, B, C, 1, 2, 3, etc.)
 """
+model="inc/jetmoe-local"
+accelerator = Accelerator()
+tokenizer=AutoTokenizer.from_pretrained(model,local_files_only=True, trust_remote_code=True)
+llm=AutoModelForCausalLM.from_pretrained(
+    model,
+    local_files_only=True,
+    trust_remote_code=True,
+    device_map="auto",
+)
 
-if '-o' in sys.argv:
-    ollamaOn=True;
-    model=sys.argv[1+sys.argv.index("-o")]
-else :
-    model="inc/jetmoe-local"
-    accelerator = Accelerator()
-    tokenizer=AutoTokenizer.from_pretrained(model,local_files_only=True, trust_remote_code=True)
-    llm=AutoModelForCausalLM.from_pretrained(model,local_files_only=True, trust_remote_code=True, device_map={"":accelerator.device})
+
 if '-c' in sys.argv:
     if (a:=sys.argv[1+sys.argv.index("-c")])=="all":
         whole_set=True
     else:
         num_rows=int(a)
-if '-t' in sys.argv:
-    thinking=True
 if '-s' in sys.argv:
     split=sys.argv[1+sys.argv.index("-s")]
 if '-ss' in sys.argv:
@@ -43,64 +40,56 @@ if '-ss' in sys.argv:
 if '-ds' in sys.argv:
     dataset=sys.argv[1+sys.argv.index("-ds")]
 
+answer_lookup={"1":"A","2":"B","3":"C","4":"D","A":"1","B":"2","C":"3","D":"4"}
 
-def send_and_grade(model: str, question: str, choices: dict[str:list[str]], answer: str):
-    q=question+'\n'+'\n'.join(map(': '.join, zip(choices["label"],choices["text"])))
+def load_data():
+    if dataset=="allenai/ai2_arc":
+        return load_dataset(dataset,subset,split=split).remove_columns("id").map(lambda data: {"choices": data["choices"]["label"]+": "+data["choices"]["text"]}).shuffle(time.time_ns())
+    elif dataset=="Rowan/hellaswag":
+        return load_dataset(dataset,split=split).remove_columns(["ctx_a","ctx_b","ind","source_id"]).rename_column("ctx","question").rename_column("endings","choices").rename_column("label","answerKey").map(lambda data: {"choices": list(map(lambda ix: answer_lookup[str(ix[0]+1)]+": "+ix[1], enumerate(data["choices"]))), "answerKey": str(int(data["answerKey"])+1)}).shuffle(time.time_ns())
+
+def send_and_grade(model: str, question: str, choices: list[str], answer: str):
+    q=question+'\n'+'\n'.join(choices)
     print(q.strip())
-    if ollamaOn:
-        response: ChatResponse = chat(model=model,messages=[
-            {
-                "role": "user",
-                "content": sysPrompt+q
-            }
-        ],think=thinking)
-        print(model,"response:",response.message.content.strip())
-        if response.message.content.strip().startswith(answer):
-            print("correct")
-            return True
-        else:
-            print("incorrect: correct answer was",answer)
+    content = [
+        {"role": "system", "content": sysPrompt},
+        {"role": "user", "content": q},
+    ]
+    start=time.time()
+    prompt = tokenizer.apply_chat_template(
+        content,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    m_input=tokenizer(prompt,return_tensors='pt').to(accelerator.device)
+    t=time.time()
+    print(t-start,"Seconds,",input_length:=m_input['input_ids'].shape[-1],"Input Tokens") 
+    gen_ids=llm.generate(
+        **m_input,
+        do_sample=True,
+        temperature=.8,
+        max_new_tokens=20,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    print(time.time()-t,"Seconds,",gen_ids.shape[1]-input_length,"Output Tokens")
+    response=tokenizer.decode(gen_ids[0][input_length:],skip_special_tokens=True).strip()
+    print(model,"response:",response)
+    if response.startswith(answer):
+        print("correct")
+        return True
+    elif response[0] in answer_lookup and answer_lookup[response[0]]==answer:
+        print("correct with replacement")
+        return True
     else:
-        content = [
-            {"role": "system", "content": sysPrompt},
-            {"role": "user", "content": q},
-        ]
-        start=time.time()
-        prompt = tokenizer.apply_chat_template(
-            content,
-            tokenize=False,
-            add_generation_prompt=True
-        )
-        m_input=tokenizer(prompt,return_tensors='pt').to(accelerator.device)
-        t=time.time()
-        print(t-start,"Seconds,",input_length:=m_input['input_ids'].shape[-1],"Input Tokens") 
-        gen_ids=llm.generate(
-            **m_input,
-            do_sample=True,
-            temperature=.8,
-            max_new_tokens=20,
-            pad_token_id=tokenizer.eos_token_id,
-        )
-        print(time.time()-t,"Seconds,",gen_ids.shape[1]-input_length,"Output Tokens")
-        response=tokenizer.decode(gen_ids[0][input_length:],skip_special_tokens=True).strip()
-        print(model,"response:",response)
-        answer_lookup={"1":"A","2":"B","3":"C","4":"D","A":"1","B":"2","C":"3","D":"4"}
-        if response.startswith(answer):
-            print("correct")
-            return True
-        elif response[0] in answer_lookup and answer_lookup[response[0]]==answer:
-            print("correct with replacement")
-            return True
-        else:
-            print("incorrect: correct answer was",answer)
+        print("incorrect: correct answer was",answer)
 
     return False
 
 if whole_set:
-    data = load_dataset(dataset,subset,split=split).remove_columns("id").shuffle(time.time_ns())
+    data = load_data()
     num_rows=sum(1 for _ in data["question"])
 else:
-    data = load_dataset(dataset,subset,split=split).remove_columns("id").shuffle(time.time_ns()).take(num_rows)
+    data = load_data().take(num_rows)
 questions = data["question"]
 choices = data["choices"]
 answers = data["answerKey"]
